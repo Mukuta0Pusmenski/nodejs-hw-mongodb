62bacd67050ec25b7faeb5d53ceefa8d3e643d8d
Mukuta0Pusmenski <167695339+Mukuta0Pusmenski@users.noreply.github.com>
2025-03-28 02:16:21 +0200

    push=005

--- src/index.js
+++ src/index.js
@@ -13,8 +13,13 @@ const startApp = async () => {
 
 startApp();
 
+require('dotenv').config();
+
+console.log('ENV VARIABLES LOADED:');
 console.log('MONGODB_URL:', process.env.MONGODB_URL);
 console.log('MONGODB_USER:', process.env.MONGODB_USER);
 console.log('MONGODB_PASSWORD:', process.env.MONGODB_PASSWORD);
 console.log('MONGODB_DB:', process.env.MONGODB_DB);
 console.log('PORT:', process.env.PORT);
+
+// Якщо якась змінна undefined — Render їх не передає.
