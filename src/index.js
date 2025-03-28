f075abc1318a292171718432e9a1ed1baf81f267
Mukuta0Pusmenski <167695339+Mukuta0Pusmenski@users.noreply.github.com>
2025-03-28 02:13:45 +0200

    Виправлення змін і оновлення серверу=4

--- src/index.js
+++ src/index.js
@@ -12,3 +12,9 @@ const startApp = async () => {
 };
 
 startApp();
+
+console.log('MONGODB_URL:', process.env.MONGODB_URL);
+console.log('MONGODB_USER:', process.env.MONGODB_USER);
+console.log('MONGODB_PASSWORD:', process.env.MONGODB_PASSWORD);
+console.log('MONGODB_DB:', process.env.MONGODB_DB);
+console.log('PORT:', process.env.PORT);
