73de1bf919aec2d173565dd2ee5d80ffaacc0755
Mukuta0Pusmenski <167695339+Mukuta0Pusmenski@users.noreply.github.com>
2025-04-06 23:48:34 +0300

    push=001

--- src/controllers/contactsController.js
+++ src/controllers/contactsController.js
@@ -70,6 +70,80 @@
 
 // export { getAllContacts, getContactById };
 
+// import { getAll, getById, addContact, updateContact, deleteContact } from '../services/contacts.js';
+// import ctrlWrapper from '../utils/ctrlWrapper.js'; // Обгортка для контролерів
+
+// const getAllContacts = async (req, res) => {
+//   const contacts = await getAll();
+//   res.status(200).json({
+//     status: 200,
+//     message: 'Successfully found contacts!',
+//     data: contacts,
+//   });
+// };
+
+// const getContactById = async (req, res) => {
+//   const { contactId } = req.params;
+//   const contact = await getById(contactId);
+
+//   if (!contact) {
+//     res.status(404).json({ message: 'Contact not found' });
+//     return;
+//   }
+
+//   res.status(200).json({
+//     status: 200,
+//     message: `Successfully found contact with id ${contactId}!`,
+//     data: contact,
+//   });
+// };
+
+// const createContact = async (req, res) => {
+//   const newContact = await addContact(req.body); // Виклик сервісу
+//   res.status(201).json({
+//     status: 201,
+//     message: 'Successfully created a contact!',
+//     data: newContact,
+//   });
+// };
+
+
+// const updateContactById = async (req, res) => {
+//   const { contactId } = req.params;
+//   const updatedContact = await updateContact(contactId, req.body);
+
+//   if (!updatedContact) {
+//     res.status(404).json({ message: 'Contact not found' });
+//     return;
+//   }
+
+//   res.status(200).json({
+//     status: 200,
+//     message: `Successfully updated contact with id ${contactId}!`,
+//     data: updatedContact,
+//   });
+// };
+
+// const deleteContactById = async (req, res) => {
+//   const { contactId } = req.params;
+//   const deletedContact = await deleteContact(contactId);
+
+//   if (!deletedContact) {
+//     res.status(404).json({ message: 'Contact not found' });
+//     return;
+//   }
+
+//   res.status(204).send(); // Без тіла відповіді
+// };
+
+// export {
+//   ctrlWrapper(getAllContacts) as getAllContacts,
+//   ctrlWrapper(getContactById) as getContactById,
+//   ctrlWrapper(createContact) as createContact,
+//   ctrlWrapper(updateContactById) as updateContactById,
+//   ctrlWrapper(deleteContactById) as deleteContactById,
+// };
+
 import { getAll, getById, addContact, updateContact, deleteContact } from '../services/contacts.js';
 import ctrlWrapper from '../utils/ctrlWrapper.js'; // Обгортка для контролерів
 
@@ -99,14 +173,13 @@ const getContactById = async (req, res) => {
 };
 
 const createContact = async (req, res) => {
-  const newContact = await addContact(req.body); // Виклик сервісу
+  const newContact = await addContact(req.body);
   res.status(201).json({
     status: 201,
     message: 'Successfully created a contact!',
     data: newContact,
   });
 };
-
 
 const updateContactById = async (req, res) => {
   const { contactId } = req.params;
@@ -136,10 +209,8 @@ const deleteContactById = async (req, res) => {
   res.status(204).send(); // Без тіла відповіді
 };
 
-export {
-  ctrlWrapper(getAllContacts) as getAllContacts,
-  ctrlWrapper(getContactById) as getContactById,
-  ctrlWrapper(createContact) as createContact,
-  ctrlWrapper(updateContactById) as updateContactById,
-  ctrlWrapper(deleteContactById) as deleteContactById,
-};
+export const getAllContacts = ctrlWrapper(getAllContacts);
+export const getContactById = ctrlWrapper(getContactById);
+export const createContact = ctrlWrapper(createContact);
+export const updateContactById = ctrlWrapper(updateContactById);
+export const deleteContactById = ctrlWrapper(deleteContactById);
