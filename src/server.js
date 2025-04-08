5028b35be57a82a8dcf034618aaf145880544c32
Mukuta0Pusmenski <167695339+Mukuta0Pusmenski@users.noreply.github.com>
2025-04-08 21:23:37 +0300

    push=002

--- src/server.js
+++ src/server.js
@@ -38,7 +38,11 @@ const setupServer = () => {
   });
 
   // const PORT = process.env.PORT;
-  const PORT = process.env.PORT || 3000; // Якщо PORT не визначено, використовується 3000
+const PORT = process.env.PORT || 3000; // Резервне значення — 3000
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
 
 
   app.listen(PORT, () => {
