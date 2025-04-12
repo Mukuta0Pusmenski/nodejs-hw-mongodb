c268374d08958623a02487c6848ea4689351e2c4
Mukuta0Pusmenski <167695339+Mukuta0Pusmenski@users.noreply.github.com>
2025-04-12 17:38:31 +0300

    push-004

--- src/server.js
+++ src/server.js
@@ -55,4 +55,3 @@ export default setupServer;
 console.log('Server setup complete.');
 console.log('Server setup complete?.'); // This line is for debugging purposes
 console.log('not Server setup complete!!');
-console.log('not Server setup complete?');
