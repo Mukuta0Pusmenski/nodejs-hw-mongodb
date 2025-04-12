eec7b270dfadd82613e98d9086449032952db3d5
Mukuta0Pusmenski <167695339+Mukuta0Pusmenski@users.noreply.github.com>
2025-04-12 17:56:42 +0300

    push-007

--- src/server.js
+++ src/server.js
@@ -26,16 +26,34 @@
 import express from 'express';
 import contactsRouter from './routers/contacts.js';
 
+// const setupServer = () => {
+//   const app = express();
+
+//   app.use(express.json());
+//   app.use('/api', contactsRouter);
+
+//   // Middleware для обробки 404
+//   app.use((req, res) => {
+//     res.status(404).json({ message: 'Not found' });
+//   });
+
 const setupServer = () => {
   const app = express();
 
   app.use(express.json());
   app.use('/api', contactsRouter);
 
-  // Middleware для обробки 404
+  // Middleware для 404
   app.use((req, res) => {
     res.status(404).json({ message: 'Not found' });
   });
+
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+
+
 
   // const PORT = process.env.PORT;
 const PORT = process.env.PORT || 3000;
@@ -66,3 +84,4 @@ export default setupServer;
 console.log('Server setup complete.');
 console.log('Server setup complete?.'); // This line is for debugging purposes # PORT=3000
 console.log('not Server setup complete!!');
+console.log(`Using port: ${process.env.PORT || 3000}`);
