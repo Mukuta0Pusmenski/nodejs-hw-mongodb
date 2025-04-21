1fbea0af6975ee69533b6675d9ff7cfe8aa78d57
Mukuta0Pusmenski <167695339+Mukuta0Pusmenski@users.noreply.github.com>
2025-04-21 22:09:18 +0300

    fix=push-002

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -103,13 +103,68 @@
 // export { getAll, getById, addContact, deleteContact, updateContact };
 
 
+// import Contact from '../models/contactModel.js';
+
+// // Отримання всіх контактів
+// const getAll = async () => {
+//   try {
+//     const contacts = await Contact.find();
+//     return contacts;
+//   } catch (error) {
+//     throw new Error('Unable to retrieve contacts: ' + error.message);
+//   }
+// };
+
+// // Отримання контакту за ID
+// const getById = async (id) => {
+//   try {
+//     const contact = await Contact.findById(id);
+//     return contact;
+//   } catch (error) {
+//     throw new Error('Unable to retrieve contact: ' + error.message);
+//   }
+// };
+
+// // Додавання нового контакту
+// const addContact = async (contactData) => {
+//   const newContact = new Contact(contactData);
+//   return await newContact.save(); // MongoDB автоматично генерує ID
+// };
+
+// // Видалення контакту за ID
+// const deleteContact = async (id) => {
+//   try {
+//     const contact = await Contact.findByIdAndDelete(id); // Видалення контакту через модель
+//     return contact; // Повертаємо видалений контакт або null, якщо не знайдено
+//   } catch (error) {
+//     throw new Error('Unable to delete contact: ' + error.message);
+//   }
+// };
+
+// // Оновлення контакту за ID
+// const updateContact = async (id, updateData) => {
+//   try {
+//     const updatedContact = await Contact.findByIdAndUpdate(id, updateData, {
+//       new: true, // Повертає оновлений документ
+//       runValidators: true, // Запускає валідацію моделі перед оновленням
+//     });
+
+//     return updatedContact; // Повертає оновлений контакт або null, якщо не знайдено
+//   } catch (error) {
+//     throw new Error('Unable to update contact: ' + error.message);
+//   }
+// };
+
+// // Експорт функцій
+// export { getAll, getById, addContact, deleteContact, updateContact };
+
 import Contact from '../models/contactModel.js';
 
 // Отримання всіх контактів
 const getAll = async () => {
   try {
     const contacts = await Contact.find();
-    return contacts;
+    return contacts; // Повертаємо всі контакти, отримані з бази
   } catch (error) {
     throw new Error('Unable to retrieve contacts: ' + error.message);
   }
@@ -119,7 +174,7 @@ const getAll = async () => {
 const getById = async (id) => {
   try {
     const contact = await Contact.findById(id);
-    return contact;
+    return contact; // Повертаємо контакт, знайдений базою
   } catch (error) {
     throw new Error('Unable to retrieve contact: ' + error.message);
   }
@@ -128,13 +183,13 @@ const getById = async (id) => {
 // Додавання нового контакту
 const addContact = async (contactData) => {
   const newContact = new Contact(contactData);
-  return await newContact.save(); // MongoDB автоматично генерує ID
+  return await newContact.save(); // Повертаємо створений контакт
 };
 
 // Видалення контакту за ID
 const deleteContact = async (id) => {
   try {
-    const contact = await Contact.findByIdAndDelete(id); // Видалення контакту через модель
+    const contact = await Contact.findByIdAndDelete(id);
     return contact; // Повертаємо видалений контакт або null, якщо не знайдено
   } catch (error) {
     throw new Error('Unable to delete contact: ' + error.message);
@@ -146,10 +201,10 @@ const updateContact = async (id, updateData) => {
   try {
     const updatedContact = await Contact.findByIdAndUpdate(id, updateData, {
       new: true, // Повертає оновлений документ
-      runValidators: true, // Запускає валідацію моделі перед оновленням
+      runValidators: true, // Вмикає валідацію перед оновленням
     });
 
-    return updatedContact; // Повертає оновлений контакт або null, якщо не знайдено
+    return updatedContact; // Повертаємо оновлений контакт або null, якщо не знайдено
   } catch (error) {
     throw new Error('Unable to update contact: ' + error.message);
   }
