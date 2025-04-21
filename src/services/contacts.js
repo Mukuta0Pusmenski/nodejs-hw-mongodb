f52e788fbd1a24c88428798e6b3531f1c552baae
Mukuta0Pusmenski <167695339+Mukuta0Pusmenski@users.noreply.github.com>
2025-04-21 21:36:16 +0300

    fix=push-001

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -33,8 +33,79 @@
 
 // export default Contact;
 
+// import Contact from '../models/contactModel.js';
+
+// const getAll = async () => {
+//   try {
+//     const contacts = await Contact.find();
+//     return contacts;
+//   } catch (error) {
+//     throw new Error('Unable to retrieve contacts: ' + error.message);
+//   }
+// };
+
+// const getById = async (id) => {
+//   try {
+//     const contact = await Contact.findById(id);
+//     return contact;
+//   } catch (error) {
+//     throw new Error('Unable to retrieve contact: ' + error.message);
+//   }
+// };
+
+// const contacts = []; // Простий масив для демонстрації. Замініть на базу даних.
+
+// // const addContact = async (contactData) => {
+// //   const newContact = {
+// //     id: contacts.length + 1, // Автоматичне генерування ID
+// //     ...contactData,
+// //   };
+// //   contacts.push(newContact); // Додаємо до списку
+// //   return newContact;
+// // };
+
+// export const addContact = async (contactData) => {
+//   const newContact = new Contact(contactData);
+//   return await newContact.save(); // Переконайся, що ID генерується базою
+// };
+
+
+// const deleteContact = async (id) => {
+//   try {
+//     const contact = await Contact.findByIdAndDelete(id); // Видалення контакту через модель
+//     return contact; // Повертаємо видалений контакт або null, якщо не знайдено
+//   } catch (error) {
+//     throw new Error('Unable to delete contact: ' + error.message);
+//   }
+// };
+
+// const updateContact = async (id, updateData) => {
+//   try {
+//     const updatedContact = await Contact.findByIdAndUpdate(id, updateData, {
+//       new: true, // Повертає оновлений документ
+//       runValidators: true, // Запускає валідацію моделі перед оновленням
+//     });
+
+//     return updatedContact; // Повертає оновлений контакт або null, якщо не знайдено
+//   } catch (error) {
+//     throw new Error('Unable to update contact: ' + error.message);
+//   }
+// };
+
+
+
+// // export { addContact };
+
+
+// // Експорт функцій
+// // export { getAll, getById };
+
+// export { getAll, getById, addContact, deleteContact, updateContact };
+
+
 import Contact from '../models/contactModel.js';
 
+// Отримання всіх контактів
 const getAll = async () => {
   try {
     const contacts = await Contact.find();
@@ -44,6 +115,7 @@ const getAll = async () => {
   }
 };
 
+// Отримання контакту за ID
 const getById = async (id) => {
   try {
     const contact = await Contact.findById(id);
@@ -53,17 +125,13 @@ const getById = async (id) => {
   }
 };
 
-const contacts = []; // Простий масив для демонстрації. Замініть на базу даних.
-
+// Додавання нового контакту
 const addContact = async (contactData) => {
-  const newContact = {
-    id: contacts.length + 1, // Автоматичне генерування ID
-    ...contactData,
-  };
-  contacts.push(newContact); // Додаємо до списку
-  return newContact;
+  const newContact = new Contact(contactData);
+  return await newContact.save(); // MongoDB автоматично генерує ID
 };
 
+// Видалення контакту за ID
 const deleteContact = async (id) => {
   try {
     const contact = await Contact.findByIdAndDelete(id); // Видалення контакту через модель
@@ -73,6 +141,7 @@ const deleteContact = async (id) => {
   }
 };
 
+// Оновлення контакту за ID
 const updateContact = async (id, updateData) => {
   try {
     const updatedContact = await Contact.findByIdAndUpdate(id, updateData, {
@@ -86,12 +155,5 @@ const updateContact = async (id, updateData) => {
   }
 };
 
-
-
-// export { addContact };
-
-
 // Експорт функцій
-// export { getAll, getById };
-
 export { getAll, getById, addContact, deleteContact, updateContact };
