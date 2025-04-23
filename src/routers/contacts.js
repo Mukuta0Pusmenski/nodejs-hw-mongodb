216a6e7bc5a47dcaccda8bb0406c95fcef76a9e3
Mukuta0Pusmenski <167695339+Mukuta0Pusmenski@users.noreply.github.com>
2025-04-23 10:52:40 +0300

    push=001

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -6,14 +6,18 @@ import {
   updateContactById,
   deleteContactById,
 } from '../controllers/contactsController.js';
+
 import ctrlWrapper from '../utils/ctrlWrapper.js';
+import { validateBody } from '../middlewares/validateBody.js'; // Додаємо валідацію body
+import { isValidId } from '../middlewares/isValidId.js'; // Додаємо перевірку ID
+import { contactSchema } from '../schemas/validationSchemas.js'; // Схема валідації
 
 const router = express.Router();
 
 router.get('/contacts', ctrlWrapper(getAllContacts));
-router.get('/contacts/:contactId', ctrlWrapper(getContactById));
-router.post('/contacts', ctrlWrapper(createContact));
-router.patch('/contacts/:contactId', ctrlWrapper(updateContactById)); // Маршрут для оновлення контакту
-router.delete('/contacts/:contactId', ctrlWrapper(deleteContactById)); // Маршрут для видалення контакту
+router.get('/contacts/:contactId', isValidId, ctrlWrapper(getContactById));
+router.post('/contacts', validateBody(contactSchema), ctrlWrapper(createContact));
+router.patch('/contacts/:contactId', isValidId, validateBody(contactSchema), ctrlWrapper(updateContactById)); // Оновлення з перевіркою ID та body
+router.delete('/contacts/:contactId', isValidId, ctrlWrapper(deleteContactById)); // Видалення з перевіркою ID
 
 export default router;
