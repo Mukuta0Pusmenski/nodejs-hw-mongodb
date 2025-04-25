ecf48da5b2994e91c8057e8e8dc24d73a3f03a5f
Mukuta0Pusmenski <167695339+Mukuta0Pusmenski@users.noreply.github.com>
2025-04-26 00:01:24 +0300

    push=002

--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -1,11 +1,26 @@
+// import Joi from 'joi';
+
+// export const validateBody = (schema) => {
+//   return (req, res, next) => {
+//     const { error } = schema.validate(req.body);
+
+//     if (error) {
+//       return res.status(400).json({ message: error.details[0].message });
+//     }
+
+//     next();
+//   };
+// };
+
 import Joi from 'joi';
 
 export const validateBody = (schema) => {
   return (req, res, next) => {
-    const { error } = schema.validate(req.body);
+    const { error } = schema.validate(req.body, { abortEarly: false }); // ✅ `abortEarly: false` повертає всі помилки
 
     if (error) {
-      return res.status(400).json({ message: error.details[0].message });
+      const errors = error.details.map(err => err.message); // ✅ Масив усіх помилок
+      return res.status(400).json({ message: "Validation error", errors });
     }
 
     next();
