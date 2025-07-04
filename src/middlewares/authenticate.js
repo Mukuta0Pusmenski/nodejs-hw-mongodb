038c67e96b235b491e0e238410a8b494c5938174
Mukuta0Pusmenski <167695339+Mukuta0Pusmenski@users.noreply.github.com>
2025-07-04 12:02:59 +0300

    push=003

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -1,3 +1,30 @@
+// import jwt from 'jsonwebtoken';
+// import createError from 'http-errors';
+// import User from '../models/user.js';
+
+// export default async function authenticate(req, res, next) {
+//   try {
+//     const authHeader = req.get('Authorization') || '';
+//     const [scheme, token] = authHeader.split(' ');
+//     if (scheme !== 'Bearer' || !token) {
+//       throw createError(401, 'Authorization token missing or malformed');
+//     }
+
+//     const payload = jwt.verify(token, process.env.ACCESS_SECRET);
+//     const user = await User.findById(payload.id);
+//     if (!user) throw createError(401, 'User not found');
+
+//     req.user = user;
+//     next();
+//   } catch (err) {
+//     if (err.name === 'TokenExpiredError') {
+//       next(createError(401, 'Access token expired'));
+//     } else {
+//       next(err);
+//     }
+//   }
+// }
+
 import jwt from 'jsonwebtoken';
 import createError from 'http-errors';
 import User from '../models/user.js';
