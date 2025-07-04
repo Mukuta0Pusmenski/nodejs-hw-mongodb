eeeecfa73a41adbb8198f08aeb522fff523f087f
Mukuta0Pusmenski <167695339+Mukuta0Pusmenski@users.noreply.github.com>
2025-07-04 18:29:46 +0300

    push=005

--- src/schemas/contact.js
+++ src/schemas/contact.js
@@ -1,10 +1,18 @@
+// // // import Joi from 'joi';
+
+// // // export const contactSchema = Joi.object({
+// // //   name:     Joi.string().min(1).required(),
+// // //   email:    Joi.string().email().required(),
+// // //   phone:    Joi.string().min(5).required(),
+// // //   message:  Joi.string().allow('').optional()
+// // // });
 // // import Joi from 'joi';
 
 // // export const contactSchema = Joi.object({
-// //   name:     Joi.string().min(1).required(),
-// //   email:    Joi.string().email().required(),
-// //   phone:    Joi.string().min(5).required(),
-// //   message:  Joi.string().allow('').optional()
+// //   name:    Joi.string().min(1).required(),
+// //   email:   Joi.string().email().required(),
+// //   phone:   Joi.string().min(5).required(),
+// //   message: Joi.string().allow('').optional()
 // // });
 // import Joi from 'joi';
 
@@ -16,9 +24,18 @@
 // });
 import Joi from 'joi';
 
-export const contactSchema = Joi.object({
+export const createContactSchema = Joi.object({
   name:    Joi.string().min(1).required(),
   email:   Joi.string().email().required(),
   phone:   Joi.string().min(5).required(),
   message: Joi.string().allow('').optional()
 });
+
+export const updateContactSchema = Joi.object({
+  name:    Joi.string().min(1).optional(),
+  email:   Joi.string().email().optional(),
+  phone:   Joi.string().min(5).optional(),
+  message: Joi.string().allow('').optional()
+})
+  .min(1)
+  .message('At least one field must be provided for update');
