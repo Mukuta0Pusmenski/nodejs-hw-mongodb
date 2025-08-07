8d695fb184c75fdf996260b6fa7451044b0e73c2
archu <gogymagik23@gmail.com>
2025-08-07 21:52:23 +0300

    push=002

--- docs/swagger.js
+++ docs/swagger.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:3000/api',
+        url: 'https://nodejs-hw-mongodb-6-kq3a.onrender.com/api',
       },
     ],
     components: {
